9210973250b5a3d77db397907841f4345f1dd0ed
iknowright <changchaishi@gmail.com>
2019-05-26 21:31:07 +0800

    chrome extension set

--- user-login-ext/repos/UserRepository.go
+++ user-login-ext/repos/UserRepository.go
@@ -1,7 +1,23 @@
 package repos
- 
+
+import (
+	app "./app"
+)
+
 func UserIsValid(uName, pwd string) bool {
     // DB simulation
+
+    n := app.news{ID: id}
+	if err := n.getNews(a.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Account not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
     _uName, _pwd, _isValid := "cihanozhan", "1234!*.", false
  
     if uName == _uName && pwd == _pwd {
